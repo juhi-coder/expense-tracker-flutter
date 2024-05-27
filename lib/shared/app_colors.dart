6c40e2ff23f5e735fc52641c64c06e0b26acd292
juhi-coder <kumarijuhimdb2@gmail.com>
2024-05-27 19:37:57 +0530

    add colors

---
+++ lib/shared/app_colors.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class AppColors {
+  static const Color black = Color.fromRGBO(15, 15, 15, 0);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color lightGreen = Color(0xFFA1DA19);
+  static const Color lightGreenShade10 = Color(0xFF99CA3D);
+  static const Color darkGrey = Color(0xFF252525);
+  static const Color greyShade10 = Color(0xB3EDF0E6);
+  static const Color greyShade20 = Color(0xFF8F9481);
+  static const Color offWhite = Color(0xFFFDF8F8);
+  static const Color whiteShade10 = Color(0xFFEFEFEF);
+  static const Color redShade10 = Color(0xFFDB3F3F);
+  static const Color darkRed = Color(0xFFB83636);
+}
